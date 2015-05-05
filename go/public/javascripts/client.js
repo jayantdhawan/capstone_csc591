0c6fbd1707cbafc3e68f201140bd697d47df0d7d
Jayant Dhawan <dhawan.jayant@gmail.com>
2015-05-05 13:00:19 -0400

    Code cleanup

--- go/public/javascripts/client.js
+++ go/public/javascripts/client.js
@@ -1,103 +1,51 @@
 
+var data, chart, options;
+var geo_chart_data, geo_chart_options, geo_chart;
+var tweet_q_i = 0, tweet_q_n, tweet_q = [];
 
-//
-// Google Chart
-//
+chart_bubble_init();
+chart_geo_init();
 
-var data;
-var chart;
-var options;
+function chart_bubble_init() {
+  // Load the Visualization API and the corechart package.
+  google.load('visualization', '1.0', {'packages':['corechart']});
+  // Set a callback to run when the Google Visualization API is loaded.
+  google.setOnLoadCallback(drawBubbleChart);
 
-// Load the Visualization API and the piechart package.
-google.load('visualization', '1.0', {'packages':['corechart']});
+  // Callback that creates and populates a data table,
+  // instantiates the chart
+  function drawBubbleChart() {
+    //console.log("Chart loaded");
+    data = new google.visualization.DataTable();
+    data.addColumn('string', 'id');
+    data.addColumn('datetime', 'time');
+    data.addColumn('number', 'rank');
+    data.addColumn('string', 'team');
 
-// Set a callback to run when the Google Visualization API is loaded.
-google.setOnLoadCallback(drawBubbleChart);
+    options = {
+      width: 1100,
+      height: 440,
+      hAxis: {title: 'Time', format: 'MMM dd, hh:mm aa', gridlines : {count : 8},
+              minorGridlines : {count : 1}},
+      vAxis: {title: 'Sentiment'},
+      bubble: {opacity: 0.5, textStyle: {fontSize: 1}},
+      //animation: {duration: 1, startup: true},
+      sizeAxis: {maxSize: 5},
+      colorAxis: {legend: {position: 'in'}}
+    };
 
-geo_init();
+    chart = new google.visualization.BubbleChart(document.getElementById('series_chart_div'));
 
-// Callback that creates and populates a data table,
-// instantiates the pie chart, passes in the data and
-// draws it.
-function drawBubbleChart() {
-  console.log("Chart loaded");
-  data = new google.visualization.DataTable();
-  data.addColumn('string', 'id');
-  data.addColumn('datetime', 'time');
-  data.addColumn('number', 'rank');
-  data.addColumn('string', 'team');
+    //google.visualization.events.addListener(chart, 'animationfinish', updateBubbleChart);
 
-  /*data.addRows([
-    ['1', new Date(2015, 4, 30, 2, 3, 01), -4, 'RCB'], ['1', new Date(2015, 4, 30, 2, 3, 01), +5, 'KKR'], 
-    ['1', new Date(2015, 5, 1, 2, 3, 03), 2, 'RCB'],     ['1', new Date(2015, 5, 1, 2, 3, 03), 0, 'KKR'],
-    ['1', new Date(2015, 5, 1, 2, 3, 04), 1, 'RCB'],     ['1',new Date(2015, 5, 1, 2, 3, 04), 2, 'KKR'],
-    ['1', new Date(2015, 5, 2, 2, 3, 08), -2, 'RCB'],  ['1',new Date(2015, 5, 2, 2, 3, 08), 5, 'KKR'], 
-    ['1', new Date(2015, 5, 2, 3, 14, 33), 0, 'RCB'],     ['1',new Date(2015, 5, 2, 3, 14, 13), 4, 'KKR'],
-    ['1', new Date(2015, 5, 2, 5, 44, 33), 1, 'RCB'],     ['1',new Date(2015, 5, 2, 5, 44, 33), -4, 'KKR']
-  ]);*/
-
-  options = {
-    width: 1100,
-    height: 440,
-    hAxis: {title: 'Time', format: 'MMM dd, hh:mm aa', gridlines : {count : 8},
-            minorGridlines : {count : 1}},
-    vAxis: {title: 'Sentiment'},
-    bubble: {opacity: 0.5, textStyle: {fontSize: 1}},
-    //animation: {duration: 1, startup: true},
-    sizeAxis: {maxSize: 5}
-  };
-
-  chart = new google.visualization.BubbleChart(document.getElementById('series_chart_div'));
-
-  //yo = google.visualization.events.addListener(chart, 'animationfinish', updateBubbleChart);
-
-  chart.draw(data, options);
+    //chart.draw(data, options);
+  }
 }
 
-var tweet_q_i = 0;
-var tweet_q_n = 0;
-var tweet_q = [];
-
-$(document).ready(function() {
-
-	sio = io();
-
-	sio.on('tweet_data', function(tweet_data) {
-		//console.log(tweet_data);
-    tweet_q[tweet_q_n] = tweet_data;
-    tweet_q_n++;
-	});
-
-  sio.on('top_tweets', function(top_tweets_array) {
-    console.log(top_tweets_array);
-    displayTopTweets(top_tweets_array);
-  });
-
-  sio.on('geo_data_team1', function(geo_data) {
-        console.log(geo_data);
-
-        for(i =0;i<geo_data.length;i++)
-          geo_chart_data.addRow([geo_data[i][0], geo_data[i][1],0]);
-        
-        geo_chart.draw(geo_chart_data, geo_chart_options);
-  });
-  sio.on('geo_data_team2', function(geo_data) {
-        console.log("asdadada");
-        for(i =0;i<geo_data.length;i++)
-          geo_chart_data.addRow([geo_data[i][0], geo_data[i][1],1]);
-        
-        geo_chart.draw(geo_chart_data, geo_chart_options);
-  })
-
-  setInterval(updateBubbleChart, 1);
-});
-
-var geo_chart_data;
-var geo_chart_options;
-var geo_chart;
-
-function geo_init () {
+function chart_geo_init () {
+  // Load the Visualization API and the geochart package.
   google.load("visualization", "1", {packages:["geochart"]});
+  // Set a callback to run when the Google Visualization API is loaded.
   google.setOnLoadCallback(drawRegionsMap);
 
   function drawRegionsMap() {
@@ -113,16 +61,15 @@ function geo_init () {
         displayMode: 'markers',
         legend: 'none',
         region: 142,
+        width: 1100,
         colorAxis: {values: [0, 1], colors: ['#e7711c', '#4374e0']} // orange to blue
     };
 
     geo_chart = new google.visualization.GeoChart(document.getElementById('regions_div'));
-    //chart.draw(geo_chart_data, geo_chart_options);
   }
 }
 
-
-function updateBubbleChart(/*id, timestamp, team1_value, team2_value*/) {
+function updateBubbleChart() {
   if (tweet_q_i < tweet_q_n) {
     $("#count").text(tweet_q_i);
 
@@ -162,65 +109,47 @@ function displayTopTweets(top_tweets_array) {
 }
 
 
-function drawScatterChart () {
+//
+// jQuery document.on(load) function
+//
+$(document).ready(function() {
 
-        var data = new google.visualization.DataTable();
-        data.addColumn('datetime', 'Student ID');
-        data.addColumn('number', 'Hours Studied');
-        data.addColumn('number', 'Final');
+  // Connect via Socket.IO
+	sio = io();
 
-        data.addRows([
-          [new Date(2015, 1, 1, 2, 3, 01), null, 67],  [new Date(2015, 1, 1, 2, 3, 02), 1, 88], 
-          [new Date(2015, 1, 1, 2, 3, 03), 2, 77],
-                [new Date(2015, 1, 1, 2, 3, 04), null, 67],  [new Date(2015, 1, 1, 2, 3, 05), 1, 88], 
-          [new Date(2015, 1, 1, 2, 3, 06), 2, 77],
-                [new Date(2015, 1, 1, 2, 3, 07), null, 67],  [new Date(2015, 1, 1, 2, 3, 08), 1, 88], 
-          [new Date(2015, 1, 1, 2, 3, 09), 2, 77],
-                [new Date(2015, 1, 1, 2, 3, 10), null, 67],  [new Date(2015, 1, 1, 2, 3, 11), 1, 88], 
-          [new Date(2015, 1, 1, 2, 3, 12), 2, 77]
-        ]);
+  // Listener on the 'tweet_data' event
+	sio.on('tweet_data', function(tweet_data) {
+		//console.log(tweet_data);
+    tweet_q[tweet_q_n] = tweet_data;
+    tweet_q_n++;
+	});
 
-        var options = {
-          chart: {
-            title: 'Sentiments analysis during the match',
-          },
-          hAxis: {
-            format: 'M/d/yy',
-            gridlines: {mincount: 10}
-          },
-          width: 1100,
-          height: 500,
-          series: {
-            0: {axis: 'hours studied'},
-            1: {axis: 'final grade'}
-          },
-          axes: {
-            y: {
-              'hours studied': {label: 'Hours Studied'},
-              'final grade': {label: 'Final Exam Grade'}
-            }
-          },
-          animation: {duration: 500, startup: true, easing: 'inAndOut'}
-        };
-
-    var chart = new google.charts.Scatter(document.getElementById('series_chart_div'));
-
-  google.visualization.events.addListener(chart, 'error', function(err) {
-    console.log(err);
+  // Listener on the 'top_tweets' event
+  sio.on('top_tweets', function(top_tweets_array) {
+    //console.log(top_tweets_array);
+    displayTopTweets(top_tweets_array);
   });
 
+  // Listener on the 'geo_data_team1' event for geocoding data of team 1
+  sio.on('geo_data_team1', function(geo_data) {
+        //console.log(geo_data);
 
-    var i = 0;
-  var updateChart = function() {
-    console.log(options.animation);
-    i++;
-    data.addRow([new Date(2015, 1, 1, 2, 3, 20+i), null, 67]);
-    chart.draw(data, options);
-  }
+        for(i =0; i<geo_data.length; i++)
+          geo_chart_data.addRow([geo_data[i][0], geo_data[i][1],0]);
 
-  //google.visualization.events.addListener(chart, 'animationfinish', updateChart);
+        geo_chart.draw(geo_chart_data, geo_chart_options);
+  });
 
-    chart.draw(data, options);
+  // Listener on the 'geo_data_team2' event for geocoding data of team 2
+  sio.on('geo_data_team2', function(geo_data) {
+        for(i =0; i<geo_data.length; i++)
+          geo_chart_data.addRow([geo_data[i][0], geo_data[i][1],1]);
 
-    setInterval(updateChart, 2000);
-}
+        geo_chart.draw(geo_chart_data, geo_chart_options);
+  })
+
+  // Call updateBubbleChart every 1 ms
+  setInterval(updateBubbleChart, 1);
+});
+
+
